fff668b66c9cefd9b87d178691da31aec4281cbe
vntralu <rafael.albano@venturus.org.br>
2021-09-30 18:56:54 -0300

    Salvando alterações

--- src/modules/users/model/User.ts
+++ src/modules/users/model/User.ts
@@ -1,7 +1,19 @@
 import { v4 as uuidV4 } from "uuid";
 
 class User {
-  // Complete aqui
+  admin: boolean;
+  email: string;
+  created_at: Date;
+  id: string;
+  name: string;
+  updated_at: Date;
+
+  constructor() {
+    this.admin = false;
+    this.created_at = new Date();
+    this.updated_at = new Date();
+    if (!this.id) this.id = uuidV4();
+  }
 }
 
 export { User };
